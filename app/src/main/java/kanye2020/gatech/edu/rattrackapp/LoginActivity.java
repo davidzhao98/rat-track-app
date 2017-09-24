8eb1a70a29212d11b30a92263066bfa7ace8404a
Julia Chen <jchen694@gatech.edu>
2017-09-24 19:12:54 -0400

    login activity

---
+++ app/src/main/java/kanye2020/gatech/edu/rattrackapp/LoginActivity.java
@@ -0,0 +1,10 @@
+package kanye2020.gatech.edu.rattrackapp;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by juliachen on 9/24/17.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+}
