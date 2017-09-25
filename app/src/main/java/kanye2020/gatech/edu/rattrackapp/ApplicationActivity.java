b0fc310867623e520cbba4d22f557507a8c3db93
Julia Chen <jchen694@gatech.edu>
2017-09-24 20:32:09 -0400

    LoginActivity

--- app/src/main/java/kanye2020/gatech/edu/rattrackapp/ApplicationActivity.java
+++ app/src/main/java/kanye2020/gatech/edu/rattrackapp/ApplicationActivity.java
@@ -1,5 +1,7 @@
 package kanye2020.gatech.edu.rattrackapp;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 /**
@@ -7,4 +9,10 @@ import android.support.v7.app.AppCompatActivity;
  */
 
 public class ApplicationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.application_view);
+    }
 }
--- app/src/main/java/kanye2020/gatech/edu/rattrackapp/LoginActivity.java
+++ app/src/main/java/kanye2020/gatech/edu/rattrackapp/LoginActivity.java
@@ -1,10 +1,13 @@
 package kanye2020.gatech.edu.rattrackapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * Created by juliachen on 9/24/17.
@@ -12,20 +15,36 @@ import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
+    EditText usernameField;
+    EditText passwordField;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
 
         //Text fields for username and password
+        usernameField = (EditText) findViewById(R.id.userNameText);
+        passwordField = (EditText) findViewById(R.id.passwordEditText2);
 
         //Buttons for login, set OnClickListener
         Button login = (Button) findViewById(R.id.loginButton2);
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                if (loginVerification()) {
+                    System.out.println("Logging in");
+                    Intent intent = new Intent(view.getContext(), ApplicationActivity.class);
+                    startActivity(intent);
+                } else {
+                    System.out.println("Fail");
+                    Toast.makeText(view.getContext(), "Incorrect login information!", Toast.LENGTH_LONG);
+                }
             }
         });
+    }
+
+    private boolean loginVerification() {
+        return usernameField.getText().equals("KanyeWest") && passwordField.getText().equals("2020");
     }
 }
