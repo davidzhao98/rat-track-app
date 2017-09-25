cf2f3e1a31f1a053ba02fbd6c1a83a4f621dae94
Julia Chen <jchen694@gatech.edu>
2017-09-24 21:06:31 -0400

    toast moved

--- app/src/main/java/kanye2020/gatech/edu/rattrackapp/LoginActivity.java
+++ app/src/main/java/kanye2020/gatech/edu/rattrackapp/LoginActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -39,8 +40,10 @@ public class LoginActivity extends AppCompatActivity {
                     startActivity(intent);
                 } else {
                     System.out.println("Fail");
-                    Toast.makeText(view.getContext(), "Incorrect login information!",
-                            Toast.LENGTH_SHORT).show();
+                    Toast toast = Toast.makeText(view.getContext(), "Incorrect login information!",
+                            Toast.LENGTH_SHORT);
+                    toast.setGravity(Gravity.BOTTOM, 0, 0);
+                    toast.show();
                 }
             }
         });
