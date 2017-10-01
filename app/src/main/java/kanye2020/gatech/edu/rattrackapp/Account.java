bd0879c135580fe28a89f1c6aeb62feb474e4926
David Zhao <david_zhao98@msn.com>
2017-10-01 15:32:22 -0400

    added account class

---
+++ app/src/main/java/kanye2020/gatech/edu/rattrackapp/Account.java
@@ -0,0 +1,15 @@
+package kanye2020.gatech.edu.rattrackapp;
+
+/**
+ * Created by David Zhao on 10/1/2017.
+ */
+
+public class Account {
+    String username;
+    String password;
+    String email;
+
+    public Account (String username, String password, String email) {
+
+    }
+}
