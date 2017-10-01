05a6c4c8b12b5505d8e98d0cb9f382537d82f262
David Zhao <david_zhao98@msn.com>
2017-10-01 17:04:57 -0400

    this doesnt work

--- app/src/main/java/kanye2020/gatech/edu/rattrackapp/RegisterActivity.java
+++ app/src/main/java/kanye2020/gatech/edu/rattrackapp/RegisterActivity.java
@@ -3,13 +3,24 @@ package kanye2020.gatech.edu.rattrackapp;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.GenericTypeIndicator;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import static android.R.attr.value;
 
 /**
  * Created by pulakazad on 9/24/17.
@@ -23,6 +34,9 @@ public class RegisterActivity extends AppCompatActivity {
     private EditText confirmPwET;
     private Spinner accountTypeSpinner;
     private Button registerAcct;
+    private ArrayList<Account> accountList;
+    private static final String TAG = "RegisterActivity";
+    private List<Account> realAccountList;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -45,10 +59,29 @@ public class RegisterActivity extends AppCompatActivity {
                     //add account to "database"?????
                     //firebase?
                     //switch to new screen/application screen
-                    Account newAccount = new Account(usernameET.getText().toString(), passwordET.getText().toString(), emailET.getText().toString());
+//                    accountList = new ArrayList<>();
+                    final Account newAccount = new Account(usernameET.getText().toString(), passwordET.getText().toString(), emailET.getText().toString());
+//                    accountList.add(newAccount);
                     FirebaseDatabase database = FirebaseDatabase.getInstance();
-                    DatabaseReference myRef = database.getReference("users/" + emailET.getText().toString());
-                    myRef.setValue(newAccount);
+                    DatabaseReference myRef = database.getReference("users");
+//                    myRef.setValue(accountList);
+                    myRef.addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            // This method is called once with the initial value and again
+                            // whenever data at this location is updated.
+                            realAccountList = (List<Account>) dataSnapshot.getValue();
+                            Log.d(TAG, "Value is: " + value);
+                            realAccountList.add(newAccount);
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError error) {
+                            // Failed to read value
+                            Log.w(TAG, "Failed to read value.", error.toException());
+                        }
+                    });
+                    myRef.setValue(realAccountList);
                 }
             }
         });
