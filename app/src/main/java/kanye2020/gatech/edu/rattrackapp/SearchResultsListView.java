2e6b51a5815606090a882d7f7224f41de2920153
Julia Chen <jchen694@gatech.edu>
2017-10-05 17:48:52 -0400

    added search results list view activity

---
+++ app/src/main/java/kanye2020/gatech/edu/rattrackapp/SearchResultsListView.java
@@ -0,0 +1,24 @@
+package kanye2020.gatech.edu.rattrackapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+/**
+ * Created by juliachen on 10/5/17.
+ */
+
+public class SearchResultsListView extends AppCompatActivity {
+
+    private ListView searchResultsLV;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.search_results);
+
+        searchResultsLV = (ListView) findViewById(R.id.searchResultsListView);
+
+    }
+}
