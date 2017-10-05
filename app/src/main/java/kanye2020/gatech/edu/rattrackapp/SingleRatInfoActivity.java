bfe1576b78b408ce6e35b311267efc976177b160
pulakazad <pulakazad@gatech.edu>
2017-10-05 18:19:27 -0400

    added SingleRatInfoActivity

---
+++ app/src/main/java/kanye2020/gatech/edu/rattrackapp/SingleRatInfoActivity.java
@@ -0,0 +1,17 @@
+package kanye2020.gatech.edu.rattrackapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by pulakazad on 10/5/17.
+ */
+
+public class SingleRatInfoActivity extends AppCompatActivity {
+
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
