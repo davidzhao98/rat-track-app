be61fc3f8f0eec312f9480461c41255f0c147b9a
Carissa <cghazalie3@gatech.edu>
2017-10-16 18:52:02 -0400

    added ReportRatActivity and activity_report_rat.xml

---
+++ app/src/main/java/kanye2020/gatech/edu/rattrackapp/ReportRatActivity.java
@@ -0,0 +1,13 @@
+package kanye2020.gatech.edu.rattrackapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReportRatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report_rat);
+    }
+}
