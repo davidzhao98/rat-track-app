8eec1b1f4af10338f35a8cf50096475c99e999d8
David Zhao <david_zhao98@msn.com>
2017-10-30 17:19:55 -0400

    added displayRat method to MapsActivity

--- app/src/main/java/kanye2020/gatech/edu/rattrackapp/MapsActivity.java
+++ app/src/main/java/kanye2020/gatech/edu/rattrackapp/MapsActivity.java
@@ -55,7 +55,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
                 Date startDate = dateFormat.parse(startDateText);
                 Date endDate = dateFormat.parse(endDateText);
-                //searchResults = RatSightingList.getInstance().sortByDate(startDate, endDate);
+//                searchResults = RatSightingList.getInstance().sortByDate(startDate, endDate);
+//                displayMap(mMap, searchResults);
                 for (int i = 0; i < 300; i++) {
                     int j = (int) (Math.random() * 100000);
                     RatSighting rat = rats.get(j);
@@ -63,18 +64,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     Date ratDate = dateFormat.parse(ratDateText);
                     if (ratDate.compareTo(startDate) >= 0 && ratDate.compareTo(endDate) <= 0) {
                         searchResults.add(rat);
-                        String id = rat.getUniqueKey();
-                        String latitude = rat.getLatitude();
-                        String longitude = rat.getLongitude();
-                        try {
-                            Double lat = Double.parseDouble(latitude);
-                            Double lng = Double.parseDouble(longitude);
-                            LatLng ratLocation = new LatLng(lat, lng);
-                            mMap.addMarker(new MarkerOptions().position(ratLocation).title("Marker of Rat " + id));
-                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ratLocation, 11));
-                        } catch(Exception e) {
-                            System.out.println("One of the rats can't be displayed");
-                        }
+                        displayRat(mMap, rat);
                     }
                 }
             } catch(Exception e) {
@@ -85,18 +75,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 int j = (int) (Math.random() * 100000);
                 RatSighting rat = rats.get(j);
                 searchResults.add(rat);
-                String id = rat.getUniqueKey();
-                String latitude = rat.getLatitude();
-                String longitude = rat.getLongitude();
-                try {
-                    Double lat = Double.parseDouble(latitude);
-                    Double lng = Double.parseDouble(longitude);
-                    LatLng ratLocation = new LatLng(lat, lng);
-                    mMap.addMarker(new MarkerOptions().position(ratLocation).title("Marker of Rat " + id));
-                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ratLocation, 11));
-                } catch(Exception e) {
-                    System.out.println("One of the rats can't be displayed");
-                }
+                displayRat(mMap, rat);
             }
         }
 //        mMap.setMinZoomPreference((float) 11);
@@ -118,5 +97,20 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 startActivity(intent);
             }
         });
+    }
+
+    public void displayRat(GoogleMap mMap, RatSighting rat) {
+        String id = rat.getUniqueKey();
+        String latitude = rat.getLatitude();
+        String longitude = rat.getLongitude();
+        try {
+            Double lat = Double.parseDouble(latitude);
+            Double lng = Double.parseDouble(longitude);
+            LatLng ratLocation = new LatLng(lat, lng);
+            mMap.addMarker(new MarkerOptions().position(ratLocation).title("Marker of Rat " + id));
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ratLocation, 11));
+        } catch(Exception e) {
+            System.out.println("One of the rats can't be displayed");
+        }
     }
 }
