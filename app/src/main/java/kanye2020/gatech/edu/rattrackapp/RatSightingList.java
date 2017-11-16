e29e4cf091b92adc1baf0c6c49565c6df7cdde05
David Zhao <david_zhao98@msn.com>
2017-11-16 18:49:09 -0500

    still errors left in RatSightingList, MapsActivity, and DateRangeActivity but those can be explained

--- app/src/main/java/kanye2020/gatech/edu/rattrackapp/RatSightingList.java
+++ app/src/main/java/kanye2020/gatech/edu/rattrackapp/RatSightingList.java
@@ -20,10 +20,11 @@ import java.util.List;
  * Created by David Zhao on 10/6/2017.
  */
 
-class RatSightingList {
-    private ArrayList<RatSighting> rats;
+final class RatSightingList {
+    private List<RatSighting> rats;
     private ArrayList<RatSighting> sample;
     private static RatSightingList instance;
+    private static final int threshold = 300;
 
     public static RatSightingList getInstance() {
         if (instance == null) {
@@ -41,42 +42,45 @@ class RatSightingList {
         myRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                System.out.println("Reading in rat data");
-                Iterator<DataSnapshot> items = dataSnapshot.getChildren().iterator();
-                //Toast.makeText(RatSightingList.this, "Rats: " + dataSnapshot.getChildrenCount(), Toast.LENGTH_SHORT).show();
-
-                //apparently rats is null
+                Iterable<DataSnapshot> itemsIterable = dataSnapshot.getChildren();
+                Iterator<DataSnapshot> items = itemsIterable.iterator();
                 if (rats == null) {
                     rats = new ArrayList<>();
                     while (items.hasNext()) {
                         DataSnapshot item = items.next();
-                        String borough, city, createdDate, incidentAddress, incidentZip, latitude,
-                                locationType, longitude, uniqueKey;
-                        borough = item.child("Borough").getValue().toString();
-                        city = item.child("City").getValue(String.class);
-                        locationType = item.child("Location Type").getValue().toString();
-                        createdDate = item.child("Created Date").getValue().toString();
-                        incidentAddress = item.child("Incident Address").getValue().toString();
-                        incidentZip = item.child("Incident Zip").getValue().toString();
-                        latitude = item.child("Latitude").getValue().toString();
-                        longitude = item.child("Longitude").getValue().toString();
-                        uniqueKey = item.child("Unique Key").getValue().toString();
-                        RatSighting entry = new RatSighting(borough, city, incidentAddress,
-                                incidentZip, locationType, createdDate, latitude, longitude, uniqueKey);
-                        rats.add(entry);
+                        DataSnapshot dataChild = item.child("Borough");
+                        String borough = dataChild.getValue(String.class);
+                        dataChild = item.child("City");
+                        String city = dataChild.getValue(String.class);
+                        dataChild = item.child("Location Type");
+                        String locationType = dataChild.getValue(String.class);
+                        dataChild = item.child("Created Date");
+                        String createdDate = dataChild.getValue(String.class);
+                        dataChild = item.child("Incident Address");
+                        String incidentAddress = dataChild.getValue(String.class);
+                        dataChild = item.child("Incident Zip");
+                        String incidentZip = dataChild.getValue(String.class);
+                        dataChild = item.child("Latitude");
+                        String latitude = dataChild.getValue(String.class);
+                        dataChild = item.child("Longitude");
+                        String longitude = dataChild.getValue(String.class);
+                        dataChild = item.child("Unique Key");
+                        String uniqueKey = dataChild.getValue(String.class);
+                        rats.add(new RatSighting(borough, city, incidentAddress,
+                                incidentZip, locationType, createdDate, latitude, longitude,
+                                uniqueKey));
+//                        rats.add(entry);
                     }
                     sample = new ArrayList<>();
                     for (int i = 0; i < 10; i++) {
                         sample.add(rats.get(i));
                     }
                 }
-
-
             }
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
-                System.out.println("Unable to read in rat data");
+//                System.out.println("Unable to read in rat data");
             }
         });
     }
@@ -84,7 +88,7 @@ class RatSightingList {
     /**
      * @return the arrayList of rats
      */
-    static ArrayList<RatSighting> getRats() {
+    static List<RatSighting> getRats() {
         return getInstance().rats;
     }
 
@@ -113,15 +117,17 @@ class RatSightingList {
         if (start.compareTo(end) > 0) {
             throw new IllegalArgumentException("Start date cannot exceed end date.");
         }
-        ArrayList<RatSighting> searchResults = new ArrayList<>();
+        List<RatSighting> searchResults = new ArrayList<>();
+        int size = getRatsSize();
         try {
             SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
-            for (int i = 0; i < 300; i++) {
-                int j = (int) (Math.random() * 100000);
+            for (int i = 0; i < threshold; i++) {
+                int j = (int) (Math.random() * size);
                 RatSighting rat = rats.get(j);
-                String ratDateText = rat.getDateTime().substring(0, 11);
+                String ratDateTime = rat.getDateTime();
+                String ratDateText = ratDateTime.substring(0, "MM/dd/yyyy".length() + 1);
                 Date ratDate = dateFormat.parse(ratDateText);
-                if (ratDate.compareTo(start) >= 0 && ratDate.compareTo(end) <= 0) {
+                if ((ratDate.compareTo(start) >= 0) && (ratDate.compareTo(end) <= 0)) {
                     searchResults.add(rat);
                 }
             }
